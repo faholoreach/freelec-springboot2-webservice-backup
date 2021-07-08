3071b3ba34c466294c76da1196fd3531c460c16d
faholoreach <faholoreach@naver.com>
2021-07-08 09:55:45 +0900

    3.4 115page commit

--- src/main/java/com/jojoldn/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldn/book/springboot/web/PostsApiController.java
@@ -1,11 +1,12 @@
 package com.jojoldn.book.springboot.web;
 
 import com.jojoldn.book.springboot.service.PostsService;
+import com.jojoldn.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldn.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldn.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +16,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto) {
+        return postService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postService.findById(id);
     }
 }
